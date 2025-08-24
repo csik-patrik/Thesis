3149477c5f2f37d2fa92c1136d883339d7ad53f3
csik-patrik <csik.patrik455@gmail.com>
2025-08-24 10:40:10 +0200

    Add: cors policy

--- ThesisApi/Program.cs
+++ ThesisApi/Program.cs
@@ -17,16 +17,23 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+{
+    builder.WithOrigins("http://localhost:5173", "http://localhost:5173").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUI(options=>options.SwaggerEndpoint("/openapi/v1.json", "ThesisApi"));
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "ThesisApi"));
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("ApiCorsPolicy");
 
 app.UseAuthorization();
 
