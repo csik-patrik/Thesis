88d2fa2cf3cce4381c6746933813ce6ade69e31c
csik-patrik <csik.patrik455@gmail.com>
2025-09-07 13:42:32 +0200

    Add: user data to home page

--- ThesisClient/src/Components/Home/Home.tsx
+++ ThesisClient/src/Components/Home/Home.tsx
@@ -1,4 +1,21 @@
-function Home() {
-  return <h1>Home page</h1>;
+import { useAuth } from "../../Auth/AuthContext";
+
+export default function Home() {
+  const { user } = useAuth();
+
+  return (
+    <h1>
+      {user ? (
+        <p>
+          Id: {user.id} <br />
+          Email: {user.email} <br />
+          Name: {user.name} <br />
+          Role: {user.role} <br />
+          Token: {user.token} <br />
+        </p>
+      ) : (
+        "No user"
+      )}
+    </h1>
+  );
 }
-export default Home;
