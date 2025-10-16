079a0768bcf3993f4d67f81daa0e1a3fe9ac8d8f
csik-patrik <csik.patrik455@gmail.com>
2025-10-16 20:18:12 +0200

    Fix: deployed mobile devices

--- ThesisClient/src/Components/MobileDevices/DeployedMobileDevicesTable.tsx
+++ ThesisClient/src/Components/MobileDevices/DeployedMobileDevicesTable.tsx
@@ -1,46 +1,32 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-interface MobileDevice {
-  id: number;
-  hostname: string;
-  mobileDeviceCategory: string;
-  imeiNumber: string;
-  serialNumber: string;
-  iosVersion: string;
-  userId: string;
-  simCard: SimCard;
-}
-
-interface SimCard {
-  id: number;
-  phoneNumber: string;
-  department: string;
-  callControlGroup: string;
-  isDataEnabled: boolean;
-  type: string;
-}
+import type {
+  SimCardResponse,
+  MobileDeviceResponse,
+} from "../../Types/MobileOrderResponse";
 
 export default function DeployedMobileDevicesTable() {
-  const [data, setData] = useState<MobileDevice[]>([]);
+  const [data, setData] = useState<MobileDeviceResponse[]>([]);
   const [categoryFilter, setCategoryFilter] = useState<string>("");
 
   useEffect(() => {
     axios
-      .get<MobileDevice[]>("http://localhost:5268/api/mobile-devices/deployed")
+      .get<MobileDeviceResponse[]>(
+        "http://localhost:5268/mobile-devices/deployed"
+      )
       .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   }, []);
 
   // Get unique device categories for the filter dropdown
   const categories = Array.from(
-    new Set(data.map((device) => device.mobileDeviceCategory))
+    new Set(data.map((device) => device.mobileDeviceCategory.name))
   );
 
   // Filter data by device category, status, and status reason
   const filteredData = data.filter((device) => {
     const categoryMatch = categoryFilter
-      ? device.mobileDeviceCategory === categoryFilter
+      ? device.mobileDeviceCategory.name === categoryFilter
       : true;
     return categoryMatch;
   });
@@ -76,12 +62,10 @@ export default function DeployedMobileDevicesTable() {
                 <th scope="col">Category</th>
                 <th scope="col">Imei number</th>
                 <th scope="col">Serial number</th>
-                <th scope="col">IOs version</th>
-                <th scope="col">User Id</th>
+                <th scope="col">User</th>
                 <th scope="col">Phone number</th>
                 <th scope="col">Call control group</th>
                 <th scope="col">Data enabled</th>
-                <th scope="col">Sim type</th>
                 <th scope="col">Actions</th>
               </tr>
             </thead>
@@ -90,15 +74,17 @@ export default function DeployedMobileDevicesTable() {
                 <tr key={d.id}>
                   <td scope="row">{d.id}</td>
                   <td>{d.hostname}</td>
-                  <td>{d.mobileDeviceCategory}</td>
+                  <td>{d.mobileDeviceCategory.name}</td>
                   <td>{d.imeiNumber}</td>
                   <td>{d.serialNumber}</td>
-                  <td>{d.iosVersion}</td>
-                  <td>{d.userId}</td>
+                  <td>{d.user.displayName}</td>
                   <td>{d.simCard.phoneNumber}</td>
-                  <td>{d.simCard.callControlGroup}</td>
-                  <td>{d.simCard.isDataEnabled ? "True" : "False"}</td>
-                  <td>{d.simCard.type}</td>
+                  <td>{d.simCard.simCallControlGroup.name}</td>
+                  <td>
+                    {d.simCard.simCallControlGroup.isDataEnabled
+                      ? "True"
+                      : "False"}
+                  </td>
                   <td>Actions</td>
                 </tr>
               ))}
