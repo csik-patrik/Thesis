a612ccdde0a459ddb674c0c081df087c5f813499
csik-patrik <csik.patrik455@gmail.com>
2025-10-20 18:28:12 +0200

    Fix: login

--- ThesisClient/src/Auth/Login.tsx
+++ ThesisClient/src/Auth/Login.tsx
@@ -22,10 +22,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post(
-        "http://localhost:5268/api/users/login",
-        formData
-      );
+      const res = await axios.post("http://localhost:5268/login", formData);
 
       // assuming your backend returns { id, email, name, token }
       login(res.data);
