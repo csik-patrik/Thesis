689f407a3a976f1f0633168c4622c138dfed2c83
csik-patrik <csik.patrik455@gmail.com>
2025-11-07 13:32:12 +0100

    Add: make decision as approver endpoint

--- ThesisApi/Interfaces/IComputerOrderRepository.cs
+++ ThesisApi/Interfaces/IComputerOrderRepository.cs
@@ -9,6 +9,7 @@ namespace ThesisApi.Interfaces
         Task<ComputerOrder?> GetByIdAsync(int id);
         Task<IEnumerable<ComputerOrder?>> GetByUsernameAsync(string username);
         Task<IEnumerable<ComputerOrder?>> GetAllWaitingForApprovalAsync(string username);
+        Task<ComputerOrder> MakeDecisionAsGroupLeaderAsync(ComputerOrder order, bool decision);
         Task<ComputerOrder> AllocateComputerToOrder(ComputerOrder order, Computer computer);
         Task<ComputerOrder> DeliverOrderAsync(ComputerOrder order);
         Task<bool> DeleteAsync(ComputerOrder order);
