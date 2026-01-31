86f885bc4f988e4c5c5b2f833029e8bb05541faf
csik-patrik <csik.patrik455@gmail.com>
2026-01-31 16:58:52 +0100

    Add: my orders links to navbar

--- ThesisClient/src/Components/NavBar/OrderItems.tsx
+++ ThesisClient/src/Components/NavBar/OrderItems.tsx
@@ -6,11 +6,11 @@ export default function OrderItems() {
     <>
       <DropDown title="Mobiles">
         <NavItem title="Create mobile order" to="/mobile-orders/create" />
-        <NavItem title="My mobile orders" to="/" />
+        <NavItem title="My mobile orders" to="/mobile-orders/my-orders" />
       </DropDown>
       <DropDown title="Computers">
         <NavItem title="Create computer order" to="/computer-orders/create" />
-        <NavItem title="My computer orders" to="/" />
+        <NavItem title="My computer orders" to="/computer-orders/my-orders" />
       </DropDown>
     </>
   );
