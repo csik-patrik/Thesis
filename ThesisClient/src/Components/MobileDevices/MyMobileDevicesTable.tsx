1dcc37ce522d868a30f9ab9476636057439b461c
csik-patrik <csik.patrik455@gmail.com>
2026-02-14 12:02:19 +0100

    Refactor my mobile devices table

--- ThesisClient/src/Components/MobileDevices/MyMobileDevicesTable.tsx
+++ ThesisClient/src/Components/MobileDevices/MyMobileDevicesTable.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import type { MobileDeviceResponse } from "../../Types/MobileTypes";
 import axios from "axios";
 import { useAuth } from "../../Auth/AuthContext";
+import Table from "../Shared/Table";
 
 export default function MyMobileDeviceTable() {
   const [data, setData] = useState<MobileDeviceResponse[]>([]);
@@ -56,88 +57,85 @@ export default function MyMobileDeviceTable() {
   });
 
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100 p-6">
+    <div className="flex flex-col items-center justify-center p-6">
       <h1 className="text-3xl font-bold mb-6">My Mobile Devices</h1>
-
-      <div className="w-full max-w-5xl bg-white border rounded-lg shadow-md p-6">
-        <div className="overflow-x-auto">
-          <table className="min-w-full border-collapse border border-gray-300">
-            <thead className="bg-gray-200">
-              <tr>
-                <th className="border px-4 py-2 text-left">Id</th>
-                <th className="border px-4 py-2 text-left">Hostname</th>
-                <th className="border px-4 py-2 text-left">
-                  Category
-                  <select
-                    id="categoryFilter"
-                    value={categoryFilter}
-                    onChange={(e) => setCategoryFilter(e.target.value)}
-                    className="block mt-1 w-full border-gray-300 rounded-md text-sm p-1"
-                  >
-                    <option value="">All</option>
-                    {categories.map((cat) => (
-                      <option key={cat} value={cat}>
-                        {cat}
-                      </option>
-                    ))}
-                  </select>
-                </th>
-                <th className="border px-4 py-2 text-left">IMEI Number</th>
-                <th className="border px-4 py-2 text-left">Serial Number</th>
-                <th className="border px-4 py-2 text-left">
-                  Status
-                  <select
-                    id="statusFilter"
-                    value={statusFilter}
-                    onChange={(e) => setStatusFilter(e.target.value)}
-                    className="block mt-1 w-full border-gray-300 rounded-md text-sm p-1"
-                  >
-                    <option value="">All</option>
-                    {statuses.map((status) => (
-                      <option key={status} value={status}>
-                        {status}
-                      </option>
-                    ))}
-                  </select>
-                </th>
-                <th className="border px-4 py-2 text-left">
-                  Status Reason
-                  <select
-                    id="statusReasonFilter"
-                    value={statusReasonFilter}
-                    onChange={(e) => setStatusReasonFilter(e.target.value)}
-                    className="block mt-1 w-full border-gray-300 rounded-md text-sm p-1"
-                  >
-                    <option value="">All</option>
-                    {statusReasons.map((reason) => (
-                      <option key={reason} value={reason}>
-                        {reason}
-                      </option>
-                    ))}
-                  </select>
-                </th>
-                <th className="border px-4 py-2 text-left">Actions</th>
+      <div className=" bg-white rounded-lg shadow-md border border-gray-200 p-6">
+        <div className="flex gap-2 mb-4 flex-col">
+          <div className="flex gap-6">
+            <div className="flex flex-col">
+              <span>Category</span>
+              <select
+                id="categoryFilter"
+                value={categoryFilter}
+                onChange={(e) => setCategoryFilter(e.target.value)}
+                className="border rounded px-2 py-1 text-sm"
+              >
+                <option value="">All</option>
+                {categories.map((cat) => (
+                  <option key={cat} value={cat}>
+                    {cat}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col">
+              <span>Status</span>
+              <select
+                id="statusFilter"
+                value={statusFilter}
+                onChange={(e) => setStatusFilter(e.target.value)}
+                className="border rounded px-2 py-1 text-sm"
+              >
+                <option value="">All</option>
+                {statuses.map((status) => (
+                  <option key={status} value={status}>
+                    {status}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col">
+              <span>Status reason</span>
+              <select
+                id="statusReasonFilter"
+                value={statusReasonFilter}
+                onChange={(e) => setStatusReasonFilter(e.target.value)}
+                className="border rounded px-2 py-1 text-sm"
+              >
+                <option value="">All</option>
+                {statusReasons.map((reason) => (
+                  <option key={reason} value={reason}>
+                    {reason}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <Table
+            headerItems={[
+              "Id",
+              "Hostname",
+              "Category",
+              "Imei number",
+              "Serial number",
+              "Status",
+              "Status reason",
+            ]}
+          >
+            {filteredData.map((d) => (
+              <tr key={d.id} className="even:bg-gray-50 border-b">
+                <td className="px-4 py-2 border-b">{d.id}</td>
+                <td className="px-4 py-2 border-b">{d.hostname}</td>
+                <td className="px-4 py-2 border-b">
+                  {d.mobileDeviceCategory.name}
+                </td>
+                <td className="px-4 py-2 border-b">{d.imeiNumber}</td>
+                <td className="px-4 py-2 border-b">{d.serialNumber}</td>
+                <td className="px-4 py-2 border-b">{d.status}</td>
+                <td className="px-4 py-2 border-b">{d.statusReason}</td>
               </tr>
-            </thead>
-            <tbody>
-              {filteredData.map((d) => (
-                <tr key={d.id} className="even:bg-gray-50">
-                  <td className="border px-4 py-2">{d.id}</td>
-                  <td className="border px-4 py-2">{d.hostname}</td>
-                  <td className="border px-4 py-2">
-                    {d.mobileDeviceCategory.name}
-                  </td>
-                  <td className="border px-4 py-2">{d.imeiNumber}</td>
-                  <td className="border px-4 py-2">{d.serialNumber}</td>
-                  <td className="border px-4 py-2">{d.status}</td>
-                  <td className="border px-4 py-2">{d.statusReason}</td>
-                  <td className="border px-4 py-2">
-                    {/* Add action buttons here if needed */}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            ))}
+          </Table>
         </div>
       </div>
     </div>
