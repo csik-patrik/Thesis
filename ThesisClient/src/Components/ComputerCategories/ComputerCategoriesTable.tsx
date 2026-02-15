230815ef364658d1c77cd1d239b27d77c08cee49
csik-patrik <csik.patrik455@gmail.com>
2026-02-15 11:36:53 +0100

    Refactor computer categories table

--- ThesisClient/src/Components/ComputerCategories/ComputerCategoriesTable.tsx
+++ ThesisClient/src/Components/ComputerCategories/ComputerCategoriesTable.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import type { ComputerCategoryResponse } from "../../Types/ComputerTypes";
 import axios from "axios";
 import { toast } from "react-toastify";
-import { Link } from "react-router-dom";
+import CustomLink from "../Shared/CustomLink";
+import Table from "../Shared/Table";
 
 export default function ComputerCategoriesTable() {
   const [computerCategories, setComputerCategories] = useState<
@@ -20,48 +21,33 @@ export default function ComputerCategoriesTable() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100 p-6">
+    <div className="flex flex-col items-center justify-center p-6">
       <h1 className="text-3xl font-bold mb-6">Mobile Device Categories</h1>
-
-      <div className="w-full max-w-3xl bg-white border rounded-lg shadow-md p-6">
-        {/* Create Button */}
-        <div className="mb-4">
-          <Link
-            to="/admin/computers/categories/create"
-            className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition"
-          >
-            Create
-          </Link>
+      <div className=" bg-white rounded-lg shadow-md border border-gray-200 p-6">
+        <div className="flex gap-2 mb-4 flex-col">
+          <div className="flex gap-2">
+            <CustomLink
+              color="green"
+              label="Create"
+              to="/admin/computers/categories/create"
+            />
+          </div>
         </div>
-
-        {/* Categories Table */}
-        <div className="overflow-x-auto">
-          <table className="min-w-full border-collapse border border-gray-300">
-            <thead className="bg-gray-200">
-              <tr>
-                <th className="border px-4 py-2 text-left">Id</th>
-                <th className="border px-4 py-2 text-left">Name</th>
-                <th className="border px-4 py-2 text-left">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {computerCategories.map((d) => (
-                <tr key={d.id} className="even:bg-gray-50">
-                  <td className="border px-4 py-2">{d.id}</td>
-                  <td className="border px-4 py-2">{d.name}</td>
-                  <td className="border px-4 py-2">
-                    <Link
-                      to={`/admin/mobile-device-categories/${d.id}`}
-                      className="bg-yellow-500 text-white px-3 py-1 rounded-md hover:bg-yellow-600 transition text-sm"
-                    >
-                      Edit
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <Table headerItems={["Id", "Name", "Actions"]}>
+          {computerCategories.map((d) => (
+            <tr key={d.id} className="hover:bg-gray-50 border-b">
+              <td className="px-4 py-2">{d.id}</td>
+              <td className="px-4 py-2">{d.name}</td>
+              <td className="px-4 py-2">
+                <CustomLink
+                  color="yellow"
+                  to={`/admin/mobile-device-categories/${d.id}`}
+                  label="Edit"
+                />
+              </td>
+            </tr>
+          ))}
+        </Table>
       </div>
     </div>
   );
