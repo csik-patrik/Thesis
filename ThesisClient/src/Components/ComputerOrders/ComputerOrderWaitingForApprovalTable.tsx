461cafd5bc748b4f409c998f3fc01e15bc92c4a1
csik-patrik <csik.patrik455@gmail.com>
2026-02-28 18:25:33 +0100

    Add: computer order approved table

--- ThesisClient/src/Components/ComputerOrders/ComputerOrderWaitingForApprovalTable.tsx
+++ ThesisClient/src/Components/ComputerOrders/ComputerOrderWaitingForApprovalTable.tsx
@@ -8,7 +8,12 @@ import CustomLink from "../Shared/CustomLink";
 
 export default function ComputerOrderWaitingForApprovalTable() {
   const { user } = useAuth();
-  const [data, setData] = useState<ComputerOrderResponse[]>([]);
+  const [ordersApproved, setOrdersApproved] = useState<ComputerOrderResponse[]>(
+    [],
+  );
+  const [ordersWaitingForApproval, setOrdersWaitingForApproval] = useState<
+    ComputerOrderResponse[]
+  >([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -22,7 +27,12 @@ export default function ComputerOrderWaitingForApprovalTable() {
             headers: { Authorization: `Bearer ${user.token}` },
           },
         );
-        setData(res.data);
+        setOrdersApproved(
+          res.data.filter((order) => order.status != "Waiting for approval"),
+        );
+        setOrdersWaitingForApproval(
+          res.data.filter((order) => order.status == "Waiting for approval"),
+        );
       } catch (err) {
         console.error("Error loading computer orders:", err);
         toast.error("Failed to load computer orders.");
@@ -42,11 +52,11 @@ export default function ComputerOrderWaitingForApprovalTable() {
     );
   }
 
-  if (data.length === 0) {
+  if (ordersApproved.length === 0 && ordersWaitingForApproval.length === 0) {
     return (
       <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-light text-center">
         <h1 className="text-muted mb-3">
-          💻 There aren't any orders waiting for approval
+          There aren't any orders waiting for approval
         </h1>
       </div>
     );
@@ -54,38 +64,85 @@ export default function ComputerOrderWaitingForApprovalTable() {
 
   return (
     <div className="flex flex-col items-center justify-center p-6">
-      <h1 className="text-3xl font-bold mb-6">
-        Computer orders waiting for approval
-      </h1>
-      <div className="bg-white rounded-lg shadow-md border border-gray-200 p-6">
-        <Table
-          headerItems={[
-            "Id",
-            "Customer name",
-            "Device type",
-            "Pickup location",
-            "Status",
-            "Actions",
-          ]}
-        >
-          {data.map((d) => (
-            <tr key={d.id} className="hover:bg-gray-50 border-b">
-              <td className="px-4 py-2">{d.id}</td>
-              <td className="px-4 py-2">{d.customer.displayName}</td>
-              <td className="px-4 py-2">{d.computerCategory.name}</td>
-              <td className="px-4 py-2">{d.pickupLocation}</td>
-              <td className="px-4 py-2">{d.status}</td>
-              <td>
-                <CustomLink
-                  color="blue"
-                  to={`/computer-orders/${d.id}`}
-                  label="View"
-                />
-              </td>
-            </tr>
-          ))}
-        </Table>
-      </div>
+      {ordersWaitingForApproval.length > 0 ? (
+        <>
+          <h1 className="text-3xl font-bold mb-6">
+            Computer orders waiting for approval
+          </h1>
+          <div className="bg-white rounded-lg shadow-md border border-gray-200 p-6">
+            <Table
+              headerItems={[
+                "Id",
+                "Customer name",
+                "Device type",
+                "Pickup location",
+                "Status",
+                "Actions",
+              ]}
+            >
+              {ordersWaitingForApproval.map((d) => (
+                <tr key={d.id} className="hover:bg-gray-50 border-b">
+                  <td className="px-4 py-2">{d.id}</td>
+                  <td className="px-4 py-2">{d.customer.displayName}</td>
+                  <td className="px-4 py-2">{d.computerCategory.name}</td>
+                  <td className="px-4 py-2">{d.pickupLocation}</td>
+                  <td className="px-4 py-2">{d.status}</td>
+                  <td className="px-4 py-2">
+                    <CustomLink
+                      color="blue"
+                      to={`/computer-orders/${d.id}`}
+                      label="View"
+                    />
+                  </td>
+                </tr>
+              ))}
+            </Table>
+          </div>
+        </>
+      ) : (
+        <h1 className="text-3xl font-bold mb-6">
+          There aren't any orders waiting for approval
+        </h1>
+      )}
+      <div className="mt-10" />
+      {ordersApproved.length > 0 ? (
+        <>
+          <h1 className="text-3xl font-bold mb-6">Approved computer orders</h1>
+          <div className="bg-white rounded-lg shadow-md border border-gray-200 p-6">
+            <Table
+              headerItems={[
+                "Id",
+                "Customer name",
+                "Device type",
+                "Pickup location",
+                "Status",
+                "Actions",
+              ]}
+            >
+              {ordersApproved.map((d) => (
+                <tr key={d.id} className="hover:bg-gray-50 border-b">
+                  <td className="px-4 py-2">{d.id}</td>
+                  <td className="px-4 py-2">{d.customer.displayName}</td>
+                  <td className="px-4 py-2">{d.computerCategory.name}</td>
+                  <td className="px-4 py-2">{d.pickupLocation}</td>
+                  <td className="px-4 py-2">{d.status}</td>
+                  <td className="px-4 py-2">
+                    <CustomLink
+                      color="blue"
+                      to={`/computer-orders/${d.id}`}
+                      label="View"
+                    />
+                  </td>
+                </tr>
+              ))}
+            </Table>
+          </div>
+        </>
+      ) : (
+        <h1 className="text-3xl font-bold mb-6">
+          There aren't any approved orders
+        </h1>
+      )}
     </div>
   );
 }
