a4e1f19383b1d61592913c365bf316f69ba499f1
csik-patrik <csik.patrik455@gmail.com>
2026-03-15 10:03:56 +0100

    Remove bg color from table layout

--- ThesisClient/src/Layouts/TableLayout.tsx
+++ ThesisClient/src/Layouts/TableLayout.tsx
@@ -17,7 +17,7 @@ export default function TableLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
+    <div className="min-h-screen p-6">
       <div className="max-w-6xl mx-auto">
         <div className="flex items-start justify-between mb-8">
           <div>
