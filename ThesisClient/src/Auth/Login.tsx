8bd28594d0a9a2638c52a98bcf1e844cb8e9685c
csik-patrik <csik.patrik455@gmail.com>
2026-04-14 11:52:28 +0200

    Add: demo user login options

--- ThesisClient/src/Auth/Login.tsx
+++ ThesisClient/src/Auth/Login.tsx
@@ -27,7 +27,75 @@ export default function Login() {
       setLoading(true);
       const res = await axios.post("http://localhost:5268/login", formData);
 
-      // assuming your backend returns { id, email, name, token }
+      login(res.data);
+
+      toast.success("Logged in successfully!");
+      navigate("/");
+    } catch (err) {
+      console.error("Login error:", err);
+      toast.error("Login error.");
+      alert("Failed to login.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmitAsDemoUser = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await axios.post("http://localhost:5268/login", {
+        email: "demo.user1@demo.com",
+        password: "PasswordDev01",
+      });
+
+      login(res.data);
+
+      toast.success("Logged in successfully!");
+      navigate("/");
+    } catch (err) {
+      console.error("Login error:", err);
+      toast.error("Login error.");
+      alert("Failed to login.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmitAsDemoAdmin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await axios.post("http://localhost:5268/login", {
+        email: "demo.user2@demo.com",
+        password: "PasswordDev02",
+      });
+
+      login(res.data);
+
+      toast.success("Logged in successfully!");
+      navigate("/");
+    } catch (err) {
+      console.error("Login error:", err);
+      toast.error("Login error.");
+      alert("Failed to login.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmitAsDemoApprover = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await axios.post("http://localhost:5268/login", {
+        email: "demo.user3@demo.com",
+        password: "PasswordDev03",
+      });
+
       login(res.data);
 
       toast.success("Logged in successfully!");
@@ -47,61 +115,78 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center p-6">
-      <div className="w-87.5 rounded-lg bg-white p-6 shadow-md">
-        <h3 className="mb-6 text-center text-xl font-semibold text-neutral-800">
-          Sign In
-        </h3>
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center justify-center">
+        <div className="w-87.5 rounded-lg bg-white p-6 shadow-md">
+          <h3 className="mb-6 text-center text-xl font-semibold text-neutral-800">
+            Sign In
+          </h3>
 
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label
-              htmlFor="email"
-              className="mb-1 block font-medium text-neutral-700"
-            >
-              Email address
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-              disabled={loading}
-              className="
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label
+                htmlFor="email"
+                className="mb-1 block font-medium text-neutral-700"
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                disabled={loading}
+                className="
               w-full rounded-md border border-neutral-300 px-3 py-2
               focus:border-neutral-500 focus:outline-none focus:ring-1 focus:ring-neutral-500
               disabled:bg-neutral-100 disabled:cursor-not-allowed
             "
-            />
-          </div>
+              />
+            </div>
 
-          <div>
-            <label
-              htmlFor="password"
-              className="mb-1 block font-medium text-neutral-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-              disabled={loading}
-              className="
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-1 block font-medium text-neutral-700"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+                disabled={loading}
+                className="
               w-full rounded-md border border-neutral-300 px-3 py-2
               focus:border-neutral-500 focus:outline-none focus:ring-1 focus:ring-neutral-500
               disabled:bg-neutral-100 disabled:cursor-not-allowed
             "
-            />
-          </div>
+              />
+            </div>
 
+            <button
+              type="submit"
+              disabled={loading}
+              className="
+            mt-2 w-full rounded-md bg-neutral-800 py-2 text-white
+            hover:bg-neutral-700 transition
+            disabled:opacity-60 disabled:cursor-not-allowed
+          "
+            >
+              {loading ? "Signing in..." : "Login"}
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="w-87.5 rounded-lg bg-white p-6 shadow-md">
           <button
-            type="submit"
+            onClick={handleSubmitAsDemoUser}
             disabled={loading}
             className="
             mt-2 w-full rounded-md bg-neutral-800 py-2 text-white
@@ -109,9 +194,33 @@ export default function Login() {
             disabled:opacity-60 disabled:cursor-not-allowed
           "
           >
-            {loading ? "Signing in..." : "Login"}
+            {loading ? "Signing in..." : "Login as demo user"}
           </button>
-        </form>
+
+          <button
+            onClick={handleSubmitAsDemoAdmin}
+            disabled={loading}
+            className="
+            mt-2 w-full rounded-md bg-neutral-800 py-2 text-white
+            hover:bg-neutral-700 transition
+            disabled:opacity-60 disabled:cursor-not-allowed
+          "
+          >
+            {loading ? "Signing in..." : "Login as demo admin"}
+          </button>
+
+          <button
+            onClick={handleSubmitAsDemoApprover}
+            disabled={loading}
+            className="
+            mt-2 w-full rounded-md bg-neutral-800 py-2 text-white
+            hover:bg-neutral-700 transition
+            disabled:opacity-60 disabled:cursor-not-allowed
+          "
+          >
+            {loading ? "Signing in..." : "Login as demo approver"}
+          </button>
+        </div>
       </div>
     </div>
   );
